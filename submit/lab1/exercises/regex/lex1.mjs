ace33e7616cf9ffa25d6dddacaa5b3203724b49b
kmoshie1 <33292245+kmoshie1@users.noreply.github.com>
2021-02-25 18:59:12 -0500

    Delete submit directory

--- submit/lab1/exercises/regex/lex1.mjs
+++
@@ -1,5 +0,0 @@
-export default {
-  $Ignore: /\s+/,  //this will be ignored.
-  INT: /\d+/,      //token with kind INT        
-  CHAR: /./,       //single char: must be last
-};
--- submit/lab1/exercises/regex/lex2.mjs
+++
@@ -1,6 +0,0 @@
-export default {
-  $Ignore: /\s+/,  //this will be ignored.
-  ID: /[a-z|A-Z|_]\w+/,
-  INT: /\d+/,      //token with kind INT        
-  CHAR: /./,       //single char: must be last
-};
