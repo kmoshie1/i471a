b486f6f80690a0e2e263ab7056d644856e4588bb
Kevin <kmoshie1@binghamton.edu>
2021-03-11 18:53:56 -0500

    completed exercise 1

--- submit/lab3/lab3-sol.scm
+++ submit/lab3/lab3-sol.scm
@@ -1,1 +1,21 @@
-(define (quadratic-roots a b c) (if (= a 0) 'error (cond ((> (- (* b b) (* 4 (* a c))) 0) (list (/ (- (sqrt (- (* b b) (* (* 4 a) c))) b) (* 2 a)) (/ (- (- (sqrt (- (* b b) (* (* 4 a) c)))) b) (* 2 a)))) ((= (- (* b b) (* 4 (* a c))) 0) (list (/ (- (sqrt (- (* b b) (* (* 4 a) c))) b) (* 2 a)))) (else '()))))
+(define quadratic-roots
+  (lambda (a b c (my-sqrt-fn sqrt)) 
+    (if (= a 0) 'error 
+      (let ([discriminant (my-sqrt-fn (- (expt b 2) (* 4 a c)))])
+        (list (/ (+ (- 0 b) discriminant) (* 2 a))
+	  (/ (- (- 0 b) discriminant) (* 2 a)))))))
+
+(define my-sqrt
+  (lambda (n (guess 1.0))
+    (if (< (abs (/ (- (* guess guess) n) n)) .0001) 
+       guess
+	 (my-sqrt n (/ (+ guess (/ n guess)) 2)))))
+	
+(define greater-than
+  (lambda (ls (v 0))
+    (if (null? ls)
+	'()
+	(let ([list1 '()])
+	(if (> (car ls) v) 
+	   ((append list1 '#t) greater-than((cdr ls) v))
+	   ((append list1 '#f) greater-than((cdr ls) v)))))))
