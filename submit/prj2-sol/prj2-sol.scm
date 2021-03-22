cb0ea0208ccfc7e41098e83930bddd0d89325fb0
Kevin <kmoshie1@binghamton.edu>
2021-03-22 10:24:48 -0400

    initial

---
+++ submit/prj2-sol/prj2-sol.scm
@@ -0,0 +1,56 @@
+;;-*- mode: scheme; -*-
+;; :set filetype=scheme
+
+
+;;Return the list resulting by multiplying each element of `list` by `x`.
+(define (mul-list list x)
+  '())  ;TODO
+
+;;Given a proper-list list of proper-lists, return the sum of the
+;;lengths of all the top-level contained lists.
+(define (sum-lengths list)
+  '())  ;TODO
+
+;;Evaluate polynomial with list of coefficients coeffs (highest-order
+;;first) at x.  The computation should reflect the traditional
+;;representation of the polynomial.
+(define (poly-eval coeffs x)
+  '())  ;TODO
+
+;;Evaluate polynomial with list of coefficients coeffs (highest-order
+;;first) at x using Horner's method.
+(define (poly-eval-horner coeffs x)
+  '())  ;TODO
+
+;;Return count of occurrences equal? to x in exp
+(define (count-occurrences exp x)
+  '())  ;TODO
+
+;;Return result of evaluating arith expression over Scheme numbers
+;;with fully parenthesized prefix binary operators 'add, 'sub, 'mul
+;;and 'div.
+(define (arith-eval exp)
+  '())  ;TODO
+
+;;Given a proper-list list of proper-lists, return sum of lengths of
+;;all the contained lists.  Must be tail-recursive.
+(define (sum-lengths-tr list)
+  '())  ;TODO
+
+;;Evaluate polynomial with list of coefficients coeffs (highest-order
+;;first) at x.  Must be tail-recursive.
+(define (poly-eval-horner-tr coeffs x)
+  '())  ;TODO
+
+;;Return the list resulting by multiplying each element of `list` by `x`.
+;;Cannot use recursion, can use one or more of `map`, `foldl`, or `foldr`.
+(define (mul-list-2 list x)
+  '())  ;TODO
+
+;;Given a proper-list list of proper-lists, return the sum of the
+;;lengths of all the contained lists.  Cannot use recursion, can use
+;;one or more of `map`, `foldl`, or `foldr`.
+(define (sum-lengths-2 list)
+  '())  ;TODO
+
+		     
