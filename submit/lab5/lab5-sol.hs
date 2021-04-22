60fc06e7002e2c77f55ac9e2f582d099a8f0cc9b
Kevin <kmoshie1@binghamton.edu>
2021-04-22 10:57:00 -0400

    all exercises done

--- submit/lab5/lab5-sol.hs
+++ submit/lab5/lab5-sol.hs
@@ -12,3 +12,22 @@ add10 = add 10
 plus5 = plus 5
 
 concHello = conc "hello"
+
+first (v, _) = v
+
+second (_, v) = v
+
+fst3 (v, _, _) = v
+
+snd3 (_, v, _) = v
+
+sumFirst2 :: Num a => [a] -> a
+sumFirst2 [] = 0
+sumFirst2 (x) = x !! 0 + x !! 1
+
+
+cartesianProduct ls1 ls2 = [ (x, y) | x <- ls1, y <- ls2 ]
+cartesianProductIf ls1 ls2 predicate = [ (x, y) | x <- ls1, y <- ls2, predicate x y ]
+
+
+oddEvenPairs n = [(x,y) | x <- [1..n], odd x, 1 <= x, y <- [1..n], even y, y <= n]
