8fccb5c37a22e52054a2dd01678c63ccee82a2fa
Kevin <kmoshie1@binghamton.edu>
2021-05-14 18:25:38 -0400

    finished fn_server

--- submit/prj5-sol/fn_server.erl
+++ submit/prj5-sol/fn_server.erl
@@ -8,14 +8,30 @@
 % start computational server which returns Fn(...Args) for
 % each client request of the form { ClientPid, Args }
 % where Args is a list of the arguments.
-start(Fn) -> 'TODO'.
+start(Fn) -> 
+	spawn(fn_server, server, [Fn]).
 
 
 % stop fn server with PID ServerPid.
-stop(ServerPid) -> 'TODO'.
+stop(ServerPid) -> 
+	ServerPid ! stop.
 
 % return next random number from server at ServerPid
-compute(ServerPid, Args) -> 'TODO'.
+compute(ServerPid, Args) ->
+	ServerPid ! { self(), Args},
+	receive
+		{ _, Result } -> Result
+	end.
 
 % TODO: auxiliary functions...
+
+server(Fn) ->
+	receive
+		{ ClientPid, Args } ->
+			Result = apply(Fn, Args),
+			ClientPid ! { self(), Result },
+			server(Fn);
+		stop ->
+			true
+	end.
 
